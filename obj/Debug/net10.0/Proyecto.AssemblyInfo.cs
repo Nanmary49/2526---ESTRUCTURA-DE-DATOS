776c975aa978d64062c1b5841f05804823d719f7
Nanmary49 <nancycampos758@gmail.com>
2025-12-03 10:26:54 -0500

    Primer commit: proyecto figuras geometricas

--- obj/Debug/net10.0/Proyecto.AssemblyInfo.cs
+++ obj/Debug/net10.0/Proyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5708d4e7986f53b1d070372b7ab57f2076a746f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

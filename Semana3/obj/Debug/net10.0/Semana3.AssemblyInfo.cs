314fd9d88f528233c589db262d0268889d0607ef
Nancy Campos <nancycampos758@gmail.com>
2026-03-17 21:05:31 -0500

    Corrección Semana13 a Semana14

--- Semana3/obj/Debug/net10.0/Semana3.AssemblyInfo.cs
+++ Semana3/obj/Debug/net10.0/Semana3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semana3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+848bd65618b33c2e64e7d93528346b557692105d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10f430c81c1789cc14e555dabf41d70363b7cc04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semana3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semana3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
